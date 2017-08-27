b0eb610276107f1eb1de45f33968e1d1db6b93ae
Pardip Bhatti <pardipbhatti@gpcoders.com>
2017-08-27 16:01:02 +0530

    @IB outlet actions for buttons

--- gpcoders-swoosh/Controller/LeagueVC.swift
+++ gpcoders-swoosh/Controller/LeagueVC.swift
@@ -9,14 +9,38 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
     }
+    
     @IBAction func unwindToLeagueVC(unwindSegue: UIStoryboardSegue) {}
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
+
+    @IBAction func onMenTap(_ sender: Any) {
+        selectLeage(leagueType: "men")
+    }
+    
+    @IBAction func onWomenTap(_ sender: Any) {
+        selectLeage(leagueType: "women")
+    }
+    
+    @IBAction func onCoedTap(_ sender: Any) {
+        selectLeage(leagueType: "coed")
+    }
+    
+    func selectLeage(leagueType: String) {
+        player.desigerLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
 }
---
+++ gpcoders-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  gpcoders-swoosh
+//
+//  Created by Pardip Bhatti on 27/08/17.
+//  Copyright © 2017 gpCoders. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desigerLeague: String?
+    var selectedSkillLever: String?
+    
+}
